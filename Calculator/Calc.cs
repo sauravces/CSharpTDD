271184bb50a268f4c45317a502c0629ad208d481
sauravbalasaheb.chou <sauravbalasaheb.chou@CESIT-LAP-0541>
2024-05-28 21:02:29 +0530

    calculator with tdd approach

---
+++ Calculator/Calc.cs
@@ -0,0 +1,24 @@
+﻿namespace Calculator
+{
+    public class Calc
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a*b;
+        }
+
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
